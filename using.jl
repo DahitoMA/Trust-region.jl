f7dbb5f08f00919ce9ee8e9998c507278719f9c9
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-07-19 11:51:49 -0400

    Create using.jl

---
+++ using.jl
@@ -0,0 +1,17 @@
+using Lint # check code
+using JLD # save variables
+using OptimizationProblems
+using NLPModels
+using LinearOperators
+using PyPlot # graph
+using BenchmarkTools
+using BenchmarkProfiles
+
+include("CG.jl")
+include("CR.jl")
+include("minres.jl")
+include("TRCG.jl")
+include("TRCR.jl")
+include("TRminres.jl")
+
+using Krylov
