0d752829684183aabf378b3ac16691c30229e4c1
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-09-28 20:40:34 -0400

    update

--- TrustRegion.jl
+++ TrustRegion.jl
@@ -24,7 +24,7 @@ function TrustRegion(model, algo, filename::String=string("result", string(algo)
 	ite = [] # number of "successfull" iterations
 	vs_ite = [] # very successfull iterations
 	mvalues = [fx] # values of the quadratic model
-	
+
 	jldname = string(filename, ".jld")
 	txtname = string(filename, ".txt")
 
@@ -34,9 +34,9 @@ function TrustRegion(model, algo, filename::String=string("result", string(algo)
 		xtrial = x + s # x_k + s
 		fxtrial = obj(model, xtrial) # f(x_k + s)
 		Δf = fxtrial - fx # f(x_k + s) - f(x_k)
-		
+
 		Hs = H * s
-    		Δm = dot(g,s) + 1/2 * dot(s,Hs) # m_k(x_k + s) - m_k(x_k)
+    		Δm = dot(g,s) + 0.5 * dot(s,Hs) # m_k(x_k + s) - m_k(x_k)
 
 		# ratio ρ
 		ρ = Δf / Δm
@@ -66,10 +66,10 @@ function TrustRegion(model, algo, filename::String=string("result", string(algo)
 
 	end
 
-	save(jldname,"x",x,"ite",ite,"fvalues",fvalues,"mvalues",mvalues)
-	X = load(jldname)
-	writedlm(txtname,X)
-	
+	# save(jldname,"x",x,"ite",ite,"fvalues",fvalues,"mvalues",mvalues)
+	# X = load(jldname)
+	# writedlm(txtname,X)
+
   	@printf("%30s %s %9s %9s %9s %9s %3s %3s %4s %4s %4s %4s %4s\n","name", "nvar", "f(x*)", "/ f(x0)", "‖∇f(x*)‖", "/ ‖∇f(x0)‖", "#f", "#g", "#Hv", "#it", "s_it", "vs_it", "reject_it")
   	@printf("%30s %d  %8.1e  %8.1e    %7.1e  %7.1e     %d   %d   %d    %d    %d   %d     %d\n", model.meta.name, n, fx, fx0, normg, normg0, neval_obj(model), neval_grad(model), neval_hprod(model), k, length(ite)-length(vs_ite),length(vs_ite), k-length(ite))
 	return x
--- using.jl
+++ using.jl
@@ -1,17 +1,20 @@
-using Lint # check code
-using JLD # save variables
+# using Lint # check code
+# using JLD # save variables
 using OptimizationProblems
 using NLPModels
 using LinearOperators
-using PyPlot # graph
-using BenchmarkTools
-using BenchmarkProfiles
+# using PyPlot # graph
+# using BenchmarkTools
+# using BenchmarkProfiles
 using MiniLogging
+# using Krylov
 
-include("CG.jl")
+# basic configuration. The root logger level is then INFO
+basic_config(MiniLogging.INFO; date_format="%Y-%m-%d %H:%M:%S")
+logger = get_logger("CR") # get root logger
+
+
+# include("CG.jl")
 include("CR.jl")
-include("minres.jl")
+# include("minres.jl")
 include("TrustRegion.jl")
-include("CRtrunc.jl")
-
-using Krylov
