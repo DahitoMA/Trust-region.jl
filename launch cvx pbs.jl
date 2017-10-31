11960a0aba1f3605f4d81226c8ed109523a8952d
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-10-31 15:44:50 -0400

    use logger to write a file

--- launch cvx pbs.jl
+++ launch cvx pbs.jl
@@ -1,12 +1,15 @@
 Problems = [arglina, arglinb, arglinc, arwhead, bdqrtic, brownden, cliff, clplatea, clplateb, clplatec, dixon3dq, dqdrtic, dqrtic, engval1, nasty, nondquar,
 palmer1c, palmer1d, palmer2c, palmer3c, palmer4c, palmer5c, palmer5d, palmer6c, palmer7c, palmer8c, power, quartc, tridia, vardim]
 
-Algos = [CG, CR, minres]
+Algos = [CG, CR]#, minres]
+
+loggerCvx = MiniLogging.Logger("loggerCvx", MiniLogging.INFO)
+push!(loggerCvx.handlers, MiniLogging.Handler(open("cvxpbs.txt", "w"), "%Y-%m-%d %H:%M:%S"))
 
 for problem in Problems
-  model = MathProgNLPModel(problem(), name=string(problem))
-  for algo in Algos
-    TrustRegion(model, algo)
-    reset!(model)
-  end
+    model = MathProgNLPModel(problem(), name=string(problem))
+    for algo in Algos
+        @info(loggerCvx, TrustRegion(model, algo))
+        reset!(model)
+    end
 end
