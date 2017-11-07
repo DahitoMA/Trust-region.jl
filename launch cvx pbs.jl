0daadcd93d6c162bc5f4eacc6093ef458ad36f6e
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-11-07 14:43:09 -0500

    save results in a txt file

--- launch cvx pbs.jl
+++ launch cvx pbs.jl
@@ -1,20 +1,17 @@
 Problems = [arglina, arglinb, arglinc, arwhead, bdqrtic, brownden, cliff, clplatea, clplateb, clplatec, dixon3dq, dqdrtic, dqrtic, engval1, nasty, nondquar,
 palmer1c, palmer1d, palmer2c, palmer3c, palmer4c, palmer5c, palmer5d, palmer6c, palmer7c, palmer8c, power, quartc, tridia, vardim]
 
-Algos = [CG, CR]
+Algos = [CG, CR, minres]
 
-D = ["model" "algo" "nvar" "f(x)" "f(x0)" "‖g(x)‖" "‖g(x0)‖" "#f" "#g" "#Hv" "#it" "#s_it" "#vs_it" "#rej_it"]
-
-# loggerCvx = MiniLogging.Logger("loggerCvx", MiniLogging.INFO)
-# push!(loggerCvx.handlers, MiniLogging.Handler(open("cvxpbs.txt", "w"), "%Y-%m-%d %H:%M:%S"))
+D = ["model     algo nvar   f(x)    f(x0)   ‖g(x)‖  ‖g(x0)‖   #f  #g  #Hv  #it #s_it #vs_it #rej_it"]
 
 for problem in Problems
     model = MathProgNLPModel(problem(), name=string(problem))
     for algo in Algos
-        # @info(loggerCvx, TrustRegion(model, algo))
         D = vcat(D, TrustRegion(model, algo))
         reset!(model)
     end
 end
 
-writedlm("cvxpbs.xls", D)
+# writedlm("cvxpbs2.txt", D)
+writedlm("cvxpbsmincg.txt", D)
