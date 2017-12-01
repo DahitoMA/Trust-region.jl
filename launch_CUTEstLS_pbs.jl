c8aed80addb80b2f012cdaf10d01124797fbf697
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-12-01 16:51:55 -0500

    add launch_CUTEstLS_pbs.jl

---
+++ launch_CUTEstLS_pbs.jl
@@ -0,0 +1,20 @@
+using CUTEst
+using Krylov
+
+Problems = open(readlines, "ProblemsLS.txt")
+Problems = Problems[2:7]
+Algos = [cgls]#, lsqr, crls, lsmr]
+
+D = ["model     algo nvar   f(x)    f(x0)   ‖g(x)‖  ‖g(x0)‖   #f  #g  #Hv  #it #s_it #vs_it #rej_it"]
+
+for problem in Problems
+    model = CUTEstModel(problem)
+    for algo in Algos
+        D = vcat(D, TrustRegionLS(model, algo))
+        println(D)
+        reset!(model)
+    end
+    finalize(model)
+end
+
+writedlm("CUTEstLSpbs.txt", D)
