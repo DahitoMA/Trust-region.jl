6b191ee338c7a730429951069859c6c405611b12
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-12-01 16:40:15 -0500

    delete atol and rtol

--- CG.jl
+++ CG.jl
@@ -1,12 +1,12 @@
 import Krylov
 
 # A truncated version of the conjugate gradient method.
-# CG(A,b,Δ,atol,rtol) solves the linear system 'A * x = b' within a region of radius Δ.
+# CG(A,b,Δ,itmax) solves the linear system 'A * x = b' within a region of radius Δ.
 
 """A truncated version of the conjugate gradient method to solve the symmetric linear system Ax=b.
 A can be positive definite or not.
 """
-function CG(A, b, Δ::Float64=10., atol::Float64=1e-8, rtol::Float64=1e-6, itmax::Int=0)
+function CG(A, b, Δ::Float64=10., itmax::Int=0; args...)
     n = size(b, 1) # size of the problem
     (size(A, 1) == n & size(A, 2) == n) || error("Inconsistent problem size")
     @info(loggerCG, @sprintf("CG: system of %d equations in %d variables", n, n))
@@ -17,7 +17,6 @@ function CG(A, b, Δ::Float64=10., atol::Float64=1e-8, rtol::Float64=1e-6, itmax
     r = -b # initial residual r = Ax-b = -b
     d = b # first descent direction
     rNorm = norm(r, 2)
-    # ϵ = atol + rtol * rNorm
     ϵ = rNorm * min(0.1, sqrt(rNorm))
     q = 0.0
     qvalues = [q] # values of the quadratic model
@@ -46,12 +45,14 @@ function CG(A, b, Δ::Float64=10., atol::Float64=1e-8, rtol::Float64=1e-6, itmax
         if Δ > 0
           # find t1 > 0 such that ‖x + t1 * d‖² = Δ²
             xNorm² = xNorm^2
+            @debug(loggerCG, @sprintf("Δ = %8.1e > 0 and ‖x‖² = %8.1e", Δ, xNorm²))
             t1 = maximum(Krylov.to_boundary(x, d, Δ; flip = false, xNorm2 = xNorm²))
+            @assert t1 > 0
 
             # if the model is not convexe or x is out of the trust region,
             # d is followed until the edge of the trust region
             if dAd ≤ 0 || α ≥ t1
-                @debug(loggerCG, @sprintf("negative curvature dAd = %8.1e or α = %8.1e ≥ t1 = %8.1e", dAd, α, t1))
+                @debug(loggerCG, @sprintf("non positive curvature dAd = %8.1e or α = %8.1e ≥ t1 = %8.1e", dAd, α, t1))
                 α = t1
                 on_boundary = true
             end
