4df1324e2323babdc96c615b5f25307cdd12b532
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-12-01 16:48:02 -0500

    update using.jl

--- using.jl
+++ using.jl
@@ -1,28 +1,31 @@
 # using Lint # check code
-using JLD # save variables
+# using JLD # save variables
 using OptimizationProblems
 using NLPModels
 using LinearOperators
-# using PyPlot # graph
+# using Plots # graph
 using BenchmarkTools
-using BenchmarkProfiles
+# using BenchmarkProfiles
 using MiniLogging
 
 # basic configuration. The root logger level is then INFO
 basic_config(MiniLogging.INFO; date_format="%Y-%m-%d %H:%M:%S")
 loggerCR = get_logger("CR")
 loggerCG = get_logger("CG")
-loggerminres = get_logger("minres")
+# loggerminres = get_logger("minres")
 loggerTR = get_logger("TrustRegion")
+loggerTRLS = get_logger("TrustRegionLS")
 loggerCR.level = MiniLogging.ERROR
-loggerTR.level = MiniLogging.ERROR
+loggerTR.level = MiniLogging.INFO
 loggerCG.level = MiniLogging.ERROR
-loggerminres.level = MiniLogging.ERROR
+# loggerminres.level = MiniLogging.ERROR
+loggerTRLS.level = MiniLogging.INFO
 
 include("CG.jl")
 include("CR.jl")
-include("minres.jl")
+# include("minres.jl")
 include("TrustRegion.jl")
+include("TrustRegionLS.jl")
 include("TRCR.jl")
 include("TRCG.jl")
-include("TRminres.jl")
+# include("TRminres.jl")
