82b3f4389f29d22314094a4a65a11c0b5b848f4a
DahitoMA <marie-ange.dahito@polymtl.ca>
2017-12-15 16:55:08 -0500

    call lsmodel in TrustRegionLS

--- launch_CUTEstLS_pbs.jl
+++ launch_CUTEstLS_pbs.jl
@@ -2,17 +2,19 @@ using CUTEst
 using Krylov
 
 Problems = open(readlines, "ProblemsLS.txt")
-Problems = Problems[1:22]
-Algos = [cgls]#, lsqr, crls, lsmr]
+Problems = Problems[8:9]
+Algos = [cgls]#[cgls, lsqr, crls, lsmr]
 
-D = ["model     algo nvar   f(x)    f(x0)   ‖g(x)‖  ‖g(x0)‖   #f  #g  #Hv  #it #s_it #vs_it #rej_it"]
+D = ["model     algo nvar   f(x)    f(x0)   ‖g(x)‖  ‖g(x0)‖   #r  #Av  #A'v  #it #s_it #vs_it #rej_it"]
 
 for problem in Problems
     model = CUTEstModel(problem)
+    lsmodel = FeasibilityResidual(model)
     for algo in Algos
-        D = vcat(D, TrustRegionLS(model, algo))
-        println(D)
-        reset!(model)
+        T = TrustRegionLS(lsmodel, algo)
+        D = vcat(D, T)
+        println(T)
+        reset!(lsmodel)
     end
     finalize(model)
 end
