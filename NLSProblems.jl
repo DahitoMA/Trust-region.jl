1c6225f7410cc6dddc6dfad0edf8b4f3d1f6a69e
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-01-26 15:09:46 -0500

    add logger

--- NLSProblems.jl
+++ NLSProblems.jl
@@ -9,20 +9,22 @@ mgh30, mgh31, mgh32, mgh33, mgh34, mgh35]
 Algos = [cgls, lsqr, crls, lsmr]
 
 D = ["model     algo nvar   f(x)    f(x0)   ‖g(x)‖  ‖g(x0)‖   #r  #Av  #A'v  #it #s_it #vs_it #rej_it"]
+@info(loggerNLS, D[1])
 
 for problem in Problems
     model = ADNLSModel(problem())
     for algo in Algos
         T = TrustRegionLS(model, algo)
-        D = vcat(D, T)
-        println(T)
+        S = @sprintf("%5s %5s %4d %8.1e %8.1e %7.1e %7.1e %4d %4d %4d %4d %4d %4d %4d", T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8], T[9], T[10], T[11], T[12], T[13], T[14])
+        @info(loggerNLS, S)
+        D = vcat(D, S)
         reset!(model)
     end
 end
+txtname = "NLSProblems.txt"
+writedlm(txtname, D)
 
-writedlm("NLSProblems.txt", D)
-
-lines = open(readlines, "NLSProblems.txt")
+lines = open(readlines, txtname)
 V = []
 i = 0
 for l in lines
@@ -30,4 +32,4 @@ for l in lines
     l = replace(l, "Generic", string("mgh", Int(ceil((i-1)/4))))
     V = vcat(V, l)
 end
-writedlm("NLSProblems.txt", V)
+writedlm(txtname, V)
