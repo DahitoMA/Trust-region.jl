00f76229406404137fd3b4214e71a643077854ed
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-01-26 15:12:25 -0500

    add logger

--- launch_CUTEstLS_pbs.jl
+++ launch_CUTEstLS_pbs.jl
@@ -2,18 +2,20 @@ using CUTEst
 using Krylov
 
 Problems = open(readlines, "ProblemsLS.txt")
-Problems = Problems[8:9]
-Algos = [cgls]#[cgls, lsqr, crls, lsmr]
+
+Algos = [cgls, lsqr, crls, lsmr]
 
 D = ["model     algo nvar   f(x)    f(x0)   ‖g(x)‖  ‖g(x0)‖   #r  #Av  #A'v  #it #s_it #vs_it #rej_it"]
+@info(loggerCUTEstLS, D[1])
 
 for problem in Problems
     model = CUTEstModel(problem)
     lsmodel = FeasibilityResidual(model)
     for algo in Algos
         T = TrustRegionLS(lsmodel, algo)
-        D = vcat(D, T)
-        println(T)
+        S = @sprintf("%5s %5s %4d %8.1e %8.1e %7.1e %7.1e %4d %4d %4d %4d %4d %4d %4d", T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8], T[9], T[10], T[11], T[12], T[13], T[14])
+        @info(loggerCUTEstLS, S)
+        D = vcat(D, S)
         reset!(lsmodel)
     end
     finalize(model)
