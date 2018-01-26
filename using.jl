cb856e8b20085ffa1402204bf60156b7d34de7f8
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-01-26 15:14:59 -0500

    update

--- using.jl
+++ using.jl
@@ -1,11 +1,11 @@
 # using Lint # check code
 # using JLD # save variables
-using OptimizationProblems
+# using OptimizationProblems
 using NLPModels
 using LinearOperators
-# using Plots # graph
+using Plots # graph
 using BenchmarkTools
-# using BenchmarkProfiles
+using BenchmarkProfiles
 using MiniLogging
 
 # basic configuration. The root logger level is then INFO
@@ -15,11 +15,17 @@ loggerCG = get_logger("CG")
 # loggerminres = get_logger("minres")
 loggerTR = get_logger("TrustRegion")
 loggerTRLS = get_logger("TrustRegionLS")
+loggerCUTEst = get_logger("launch_CUTEst_pbs")
+loggerCUTEstLS = get_logger("launch_CUTEstLS_pbs")
+loggerind = get_logger("launch_ind_pbs")
+loggercvx = get_logger("launch_cvx_pbs")
+loggerNLS = get_logger("NLSProblems")
+
 loggerCR.level = MiniLogging.ERROR
-loggerTR.level = MiniLogging.INFO
+loggerTR.level = MiniLogging.ERROR
 loggerCG.level = MiniLogging.ERROR
 # loggerminres.level = MiniLogging.ERROR
-loggerTRLS.level = MiniLogging.INFO
+loggerTRLS.level = MiniLogging.ERROR
 
 include("CG.jl")
 include("CR.jl")
@@ -29,3 +35,7 @@ include("TrustRegionLS.jl")
 include("TRCR.jl")
 include("TRCG.jl")
 # include("TRminres.jl")
+include("TRCGLS.jl")
+include("TRCRLS.jl")
+include("TRLSQR.jl")
+include("TRLSMR.jl")
