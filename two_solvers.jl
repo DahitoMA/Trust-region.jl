73ab3e82951acf2cf50af5ca46bc1ca210645070
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-04-16 12:43:09 -0400

    use non convex pbs

--- two_solvers.jl
+++ two_solvers.jl
@@ -4,16 +4,24 @@ using Krylov
 using OptimizationProblems
 
 # non convex problems
-Problems = [AMPGO02, AMPGO03, AMPGO04, AMPGO05, AMPGO06, AMPGO08, AMPGO09,
-AMPGO10, AMPGO11, AMPGO12, AMPGO13, AMPGO14, AMPGO15, AMPGO18,
-AMPGO20, AMPGO21, AMPGO22,
-beale, brownbs, broydn7d, brybnd,
+# Problems = [AMPGO02, AMPGO03, AMPGO04, AMPGO05, AMPGO06, AMPGO08, AMPGO09,
+# AMPGO10, AMPGO11, AMPGO12, AMPGO13, AMPGO14, AMPGO15, AMPGO18,
+# AMPGO20, AMPGO21, AMPGO22,
+# beale, brownbs, broydn7d, brybnd,
+# chainwoo, chnrosnb_mod, cosine, cragglvy, curly, dixmaane, dixmaanf, dixmaang,
+# dixmaanh, dixmaani, dixmaanj, dixmaank, dixmaanl, dixmaanm, dixmaann, dixmaano, dixmaanp,
+# Dus2_1, Dus2_3, Dus2_9, Duscube,
+# edensch, eg2, errinros_mod, extrosnb, fletcbv2, fletcbv3_mod, fletchcr, fminsrf2, freuroth, genhumps, genrose, genrose_nash,
+# indef_mod, liarwhd, meyer3, morebv, ncb20, ncb20b, noncvxu2, noncvxun, nondia, penalty2, powellsg, schmvett,
+# scosine, Shpak1, Shpak2, Shpak3, Shpak4, Shpak5, Shpak6, sinquad, sparsine, sparsqur, srosenbr, tointgss, tquartic, woods]
+# only if dimension > 1
+Problems = [beale, brownbs, broydn7d, brybnd,
 chainwoo, chnrosnb_mod, cosine, cragglvy, curly, dixmaane, dixmaanf, dixmaang,
 dixmaanh, dixmaani, dixmaanj, dixmaank, dixmaanl, dixmaanm, dixmaann, dixmaano, dixmaanp,
-Dus2_1, Dus2_3, Dus2_9, Duscube,
 edensch, eg2, errinros_mod, extrosnb, fletcbv2, fletcbv3_mod, fletchcr, fminsrf2, freuroth, genhumps, genrose, genrose_nash,
 indef_mod, liarwhd, meyer3, morebv, ncb20, ncb20b, noncvxu2, noncvxun, nondia, penalty2, powellsg, schmvett,
-scosine, Shpak1, Shpak2, Shpak3, Shpak4, Shpak5, Shpak6, sinquad, sparsine, sparsqur, srosenbr, tointgss, tquartic, woods]
+scosine, sinquad, sparsine, sparsqur, srosenbr, tointgss, tquartic, woods]
+
 
 # convex problems
 # Problems = [arglina, arglinb, arglinc, arwhead, bdqrtic, brownden, cliff, clplatea, clplateb, clplatec, dixon3dq, dqdrtic, dqrtic, engval1, nasty, nondquar,
@@ -58,10 +66,10 @@ end
 
 
 stats = two_solvers()
-tf = font(17) # titlefont
-f = font(15)
+tf = font(18) # titlefont
+f = font(17)
 # pb_type = "CUTEst problems"
-pb_type = "convex problems"
+pb_type = "non-convex problems"
 algo_used = "CG and CR"
 p = performance_profile(hcat([p[:, 1] for p in values(stats)]...),
                             collect(String, [string(s) for s in keys(stats)]),
