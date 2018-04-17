81fe48436b94d1e975ba7437bb9322909c3b9c2a
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-04-17 14:54:10 -0400

    plot and save all profiles

--- two_solvers.jl
+++ two_solvers.jl
@@ -66,36 +66,36 @@ end
 
 
 stats = two_solvers()
-tf = font(18) # titlefont
-f = font(17)
+tf = font(13) # titlefont
+f = font(12)
 # pb_type = "CUTEst problems"
 pb_type = "non-convex problems"
 algo_used = "CG and CR"
 p = performance_profile(hcat([p[:, 1] for p in values(stats)]...),
                             collect(String, [string(s) for s in keys(stats)]),
                             title=string("Performance profile : #f in the resolution of ", pb_type, " with ", algo_used),
-                            titlefont = tf, legendfont = f, guidefont = f, size = (800,800)) # Profile for #f
+                            titlefont = tf, legendfont = f, guidefont = f, size = (800,800), legend=:bottomright) # Profile for #f
 
-# Plots.savefig(p, string("profil_f_", pb_type, ".pdf"))
+Plots.savefig(p, string("profil_f_", pb_type, ".pdf"))
 
-# p = performance_profile(hcat([p[:, 2] for p in values(stats)]...),
-#                             collect(String, [string(s) for s in keys(stats)]),
-#                             title=string("Performance profile : #g in the resolution of ", pb_type, " with ", algo_used),
-#                             titlefont = tf, legendfont = f, guidefont = f, size = (800,800)) # Profile for #g
-#
-# Plots.savefig(p, string("profil_g_", pb_type, ".pdf"))
-#
-#
-# p = performance_profile(hcat([p[:, 3] for p in values(stats)]...),
-#                             collect(String, [string(s) for s in keys(stats)]),
-#                             title=string("Performance profile : #Hv in the resolution of ", pb_type, " with ", algo_used),
-#                             titlefont = tf, legendfont = f, guidefont = f, size = (800,800)) # Profile for #Hv
-#
-# Plots.savefig(p, string("profil_Hv_", pb_type, ".pdf"))
-#
-# p = performance_profile(hcat([sum(p, 2) for p in values(stats)]...),
-#                             collect(String, [string(s) for s in keys(stats)]),
-#                             title=string("Performance profile : #f + #g  + #Hv in the resolution of ", pb_type, " with ", algo_used),
-#                             titlefont = tf, legendfont = f, guidefont = f, size = (800,800)) # Profile for #f + #g + #Hv
-#
-# Plots.savefig(p, string("profil_f+g+Hv_", pb_type, ".pdf"))
+p = performance_profile(hcat([p[:, 2] for p in values(stats)]...),
+                            collect(String, [string(s) for s in keys(stats)]),
+                            title=string("Performance profile : #g in the resolution of ", pb_type, " with ", algo_used),
+                            titlefont = tf, legendfont = f, guidefont = f, size = (800,800), legend=:bottomright) # Profile for #g
+
+Plots.savefig(p, string("profil_g_", pb_type, ".pdf"))
+
+
+p = performance_profile(hcat([p[:, 3] for p in values(stats)]...),
+                            collect(String, [string(s) for s in keys(stats)]),
+                            title=string("Performance profile : #Hv in the resolution of ", pb_type, " with ", algo_used),
+                            titlefont = tf, legendfont = f, guidefont = f, size = (800,800), legend=:bottomright) # Profile for #Hv
+
+Plots.savefig(p, string("profil_Hv_", pb_type, ".pdf"))
+
+p = performance_profile(hcat([sum(p, 2) for p in values(stats)]...),
+                            collect(String, [string(s) for s in keys(stats)]),
+                            title=string("Performance profile : #f + #g  + #Hv in the resolution of ", pb_type, " with ", algo_used),
+                            titlefont = tf, legendfont = f, guidefont = f, size = (800,800), legend=:bottomright) # Profile for #f + #g + #Hv
+
+Plots.savefig(p, string("profil_f+g+Hv_", pb_type, ".pdf"))
