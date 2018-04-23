2990cf10a24515b8b27bd0b96b9a65072af809d0
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-04-23 16:12:14 -0400

    update

--- using.jl
+++ using.jl
@@ -15,7 +15,12 @@ loggerCG = get_logger("CG")
 # loggerminres = get_logger("minres")
 loggerTR = get_logger("TrustRegion")
 loggerTRLS = get_logger("TrustRegionLS")
+loggerTRCUTEst = get_logger("TrustRegionCUTEst")
+loggerTRts = get_logger("TrustRegion_two_solvers")
+
 loggerCUTEst = get_logger("launch_CUTEst_pbs")
+loggerCUTEstCG = get_logger("launch_CUTEst_CG")
+loggerCUTEstCR = get_logger("launch_CUTEst_CR")
 loggerCUTEstLS = get_logger("launch_CUTEstLS_pbs")
 loggerind = get_logger("launch_ind_pbs")
 loggercvx = get_logger("launch_cvx_pbs")
@@ -23,6 +28,8 @@ loggerNLS = get_logger("NLSProblems")
 
 loggerCR.level = MiniLogging.ERROR
 loggerTR.level = MiniLogging.ERROR
+loggerTRCUTEst.level = MiniLogging.ERROR
+loggerTRts.level = MiniLogging.ERROR
 loggerCG.level = MiniLogging.ERROR
 # loggerminres.level = MiniLogging.ERROR
 loggerTRLS.level = MiniLogging.ERROR
@@ -31,9 +38,13 @@ include("CG.jl")
 include("CR.jl")
 # include("minres.jl")
 include("TrustRegion.jl")
+include("TrustRegionCUTEst.jl")
+include("TrustRegion_two_solvers.jl")
 include("TrustRegionLS.jl")
 include("TRCR.jl")
 include("TRCG.jl")
+include("TRCG_CUTEst.jl")
+include("TRCR_CUTEst.jl")
 # include("TRminres.jl")
 include("TRCGLS.jl")
 include("TRCRLS.jl")
