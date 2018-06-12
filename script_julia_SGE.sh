826626292bd91b8c82afae2c973e6a65b655066a
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-06-12 17:42:53 -0400

    put .e and .o in folders

--- script_julia_SGE.sh
+++ script_julia_SGE.sh
@@ -2,7 +2,7 @@
 # Pour lancer le fichier tu fais bash script_julia_SGE.sh
 # Tu peux aussi faire chmod +x script_julia_SGE.sh une seule fois puis faire ./script_julia_SGE
 
-for (( i=1; i <= 24; i++ ))
+for (( i=1; i <= 105; i++ ))
 # -b pour dire si tu lances un binaire ou un script (réponse y ou n)
 # -cwd pour dire que tu travailles dans le répertoire courant (Current Work Directory)
 # -V pour faire passer les variables d'environnement
@@ -10,8 +10,7 @@ for (( i=1; i <= 24; i++ ))
 # -o pour désigner le fichier qui contient le STDOUT
 do
   hostname
-	qsub -V -cwd -b y julia launch_CUTEstLS_pbs.jl $i
-	# qsub -V -cwd -b y julia somme.jl $i 1000
+  qsub -V -cwd -e STDERR/$i.txt -o STDOUT/$i.txt -b y julia launch_CUTEst_pbs.jl $i
+  sleep 50
 done
 # Attention les paramètres que tu fais passer à Julia sont dans la variable ARGS et de type String!
-# Bon courage
